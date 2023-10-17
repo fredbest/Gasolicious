9890ca7f7e4807298587cbe6d8518a22b144f3d0
fredbest <fdefeyter@gmail.com>
2023-10-16 20:58:37 -0500

    Initial commit of Drupal codebase

---
+++ web/sites/default/files/js/js_I9nKYEe0TXQtnwandgQ7dKtzGyN89oqGcI7XCXVYlZI.js
@@ -0,0 +1,2 @@
+/* @license GNU-GPL-2.0-or-later https://www.drupal.org/licensing/faq */
+(function(Drupal){'use strict';Drupal.behaviors.gasolicious={attach(context,settings){console.log('It works!');}};}(Drupal));;
---
+++ web/themes/gasolicious/js/gasolicious.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * Gasolicious behaviors.
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.gasolicious = {
+    attach (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (Drupal));
